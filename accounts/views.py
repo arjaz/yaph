ec3ca6e090ac82cb939c7f04ce25adb4d634e865
Arjaz <arjaz@protonmail.com>
2019-01-27 01:47:35 +0200

    Templates added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def sign_up(request):
+    return render(request, 'accounts/sign_up.html')
+
+def log_in(request):
+    return render(request, 'accounts/log_in.html')
+
+def log_out(request):
+    # TODO: log out and route to home page
+    return render(request, 'accounts/sign_up.html')
