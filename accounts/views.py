debb523c203c61ca9cf37ef217ed746320afc98f
Arjaz <arjaz@protonmail.com>
2019-02-02 03:31:02 +0200

    Auth and products stuff

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def sign_up(request):
-    return render(request, 'accounts/sign_up.html')
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/sign_up.html', {'error': 'Username has already been taken.'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('index')
+        else:
+            return render(request, 'accounts/sign_up.html', {'error': 'Passwords must match.'})
+    else:
+        return render(request, 'accounts/sign_up.html')
 
 def log_in(request):
-    return render(request, 'accounts/log_in.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'accounts/log_in.html', {'error': 'Username or password is incorrect.'})
+    else:
+        return render(request, 'accounts/log_in.html')
 
 def log_out(request):
-    # TODO: log out and route to home page
-    return render(request, 'accounts/sign_up.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('index')
--- yaph/urls.py
+++ yaph/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from products import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls')),
     path('', views.index, name='index'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
